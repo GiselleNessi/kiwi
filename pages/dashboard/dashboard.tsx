d422b11eff0343eb68fd66cfa1766f78ea1098e3
Giselle <gisecn@gmail.com>
2023-04-29 01:38:22 +0200

    updates mobile menu

--- pages/dashboard/dashboard.tsx
+++ pages/dashboard/dashboard.tsx
@@ -21,8 +21,8 @@ import {
 } from "@heroicons/react/24/outline";
 import { ChildProcess } from "child_process";
 import Image from "next/image";
-import profileImage from "../../public/favicon.ico"
-import homeImg from "../../public/home.jpeg"
+import profileImage from "../../public/favicon.ico";
+import homeImg from "../../public/home.jpeg";
 
 const navigation = [
   { name: "Inicio", href: "/", icon: HomeIcon, current: false },
@@ -31,9 +31,8 @@ const navigation = [
     href: "https://t.me/+h34sjsoM2ZllNzZh",
     icon: UsersIcon,
     current: false,
-    target: "_blank", 
+    target: "_blank",
   },
-  
 ];
 const teams = [
   {
@@ -135,7 +134,19 @@ export default function Dashboard({ children }: Props) {
                 </Transition.Child>
                 {/* Sidebar component, swap this element with another sidebar if you like */}
                 <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-zinc-950 px-6 pb-2 ring-1 ring-white/10">
-                  <div className="flex h-16 shrink-0 items-center">🥝</div>
+                  <div className="flex h-16 shrink-0 items-center">
+                  <Image
+                  src={profileImage}
+                  alt="Logo"
+                  width={8}
+                  height={8}
+                  className="h-8 w-8 rounded-full bg-gray-800"
+                />
+
+                <p className="ml-4 text-sm text-white leading-8">
+                  Bienvenidx {address?.slice(0, 6)}...{address?.slice(-4)}{" "}
+                </p>
+                  </div>
                   <nav className="flex flex-1 flex-col">
                     <ul role="list" className="flex flex-1 flex-col gap-y-7">
                       <li>
@@ -161,8 +172,42 @@ export default function Dashboard({ children }: Props) {
                               </a>
                             </li>
                           ))}
+                               
+                {navigation.map((item) => (
+                  <li key={item.name}>
+                    <a
+                      href={item.href}
+                      className={classNames(
+                        item.current
+                          ? "bg-gray-800 text-white"
+                          : "text-gray-400 hover:text-white hover:bg-gray-800",
+                        "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                      )}
+                      target={item.target}
+                    >
+                      <item.icon
+                        className="h-6 w-6 shrink-0"
+                        aria-hidden="true"
+                      />
+                      {item.name}
+                    </a>
+                  </li>
+                ))}
                         </ul>
                       </li>
+
+               
+            
+
+              <li className="mb-6">
+                <button
+                  onClick={handleLogout}
+                  type="button"
+                  className="rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                >
+                  Logout
+                </button>
+              </li>
                     </ul>
                   </nav>
                 </div>
@@ -176,33 +221,20 @@ export default function Dashboard({ children }: Props) {
       <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
         {/* Sidebar component, swap this element with another sidebar if you like */}
         <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-zinc-950 px-6">
-
           <nav className="flex flex-1 flex-col">
             <ul role="list" className="flex flex-1 flex-col gap-y-7">
               <li className="mt-4">
-                <a
-                  href="#"
-                  className="flex items-center gap-x-4 py-3 text-sm font-semibold leading-6 text-white hover:bg-gray-800"
-                >
-                 
-                   <Image
-                src={profileImage}
-                alt="Logo"
-                width={8}
-                height={8}
-                className="h-8 w-8 rounded-full bg-gray-800"
-              />
-                  <span className="sr-only">Your profile</span>
-                  <span aria-hidden="true">
-                  <p className="text-sm leading-8">
-                Bienvenidx
-              
-                  {" "}
-                  {address?.slice(0, 6)}...{address?.slice(-4)}{" "}
-              
-              </p>
-              </span>
-                </a>
+                <Image
+                  src={profileImage}
+                  alt="Logo"
+                  width={8}
+                  height={8}
+                  className="h-8 w-8 rounded-full bg-gray-800"
+                />
+
+                <p className="text-sm leading-8">
+                  Bienvenidx {address?.slice(0, 6)}...{address?.slice(-4)}{" "}
+                </p>
               </li>
               <li>
                 <div className="text-xs font-semibold leading-6 text-gray-400">
@@ -279,39 +311,39 @@ export default function Dashboard({ children }: Props) {
         <div className="flex-1 text-sm font-semibold leading-6 text-white">
           Menu
         </div>
-       
       </div>
 
       <main className="py-10 lg:pl-72">
         <div className="px-4 sm:px-6 lg:px-8">
           <div>
-            {children ? <div>{children}</div> : <>
-            
-            <div className="text-center pt-12 md:pb-16">
-              <h1
-                className="text-4xl md:text-5xl font-extrabold leading-tighter tracking-tighter mb-4"
-                data-aos="zoom-y-out"
-              >
-                Móntate en la nave 🛸 
-                <br/>comiendo Kiwi 🥝
-                
-              </h1>
-              <div className="text-center">
-  <div className="w-full max-w-3xl mx-auto">
-    <Image
-      src={homeImg}
-      alt="Logo"
-      width={600}
-      height={600}
-      className="mx-auto"
-      style={{ objectFit: "cover", objectPosition: "center" }}
-    />
-  </div>
-</div>
-
-
-            </div></>
-            }
+            {children ? (
+              <div>{children}</div>
+            ) : (
+              <>
+                <div className="text-center pt-12 md:pb-16">
+                  <h1
+                    className="text-4xl md:text-5xl font-extrabold leading-tighter tracking-tighter mb-4"
+                    data-aos="zoom-y-out"
+                  >
+                    Móntate en la nave 🛸
+                    <br />
+                    comiendo Kiwi 🥝
+                  </h1>
+                  <div className="text-center">
+                    <div className="w-full max-w-3xl mx-auto">
+                      <Image
+                        src={homeImg}
+                        alt="Logo"
+                        width={600}
+                        height={600}
+                        className="mx-auto"
+                        style={{ objectFit: "cover", objectPosition: "center" }}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </main>
