4b93e48431f6ad4b3421c1f1134bb3f5e4ea4e5e
Giselle <gisecn@gmail.com>
2023-04-29 01:26:16 +0200

    updated mobile menu

--- pages/dashboard/dashboard.tsx
+++ pages/dashboard/dashboard.tsx
@@ -267,7 +267,7 @@ export default function Dashboard({ children }: Props) {
         </div>
       </div>
 
-      <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-gray-900 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+      <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-zinc-950 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
         <button
           type="button"
           className="-m-2.5 p-2.5 text-gray-400 lg:hidden"
@@ -279,14 +279,7 @@ export default function Dashboard({ children }: Props) {
         <div className="flex-1 text-sm font-semibold leading-6 text-white">
           Menu
         </div>
-        <a href="#">
-          <span className="sr-only">Your profile</span>
-          <img
-            className="h-8 w-8 rounded-full bg-gray-800"
-            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-            alt=""
-          />
-        </a>
+       
       </div>
 
       <main className="py-10 lg:pl-72">
