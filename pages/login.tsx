d7b5da47bb1674583acecd398446ddf29a593461
Giselle <gisecn@gmail.com>
2023-05-30 15:23:22 +0200

    updated button style

--- pages/login.tsx
+++ pages/login.tsx
@@ -75,9 +75,11 @@ export default function Login() {
                     loginOptional: false,
                   }}
                 />
+        
                 <a
-                  href="#"
-                  className="text-sm font-semibold leading-6 text-gray-100"
+                  href="https://calendly.com/cryptonikasdao/meeting-cks-founders"
+                  target="_blank"
+                  className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2  dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
                 >
                   ¿Eres creador? <span aria-hidden="true">→</span>
                 </a>
