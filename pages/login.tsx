1ded29dd21f3d2ec91085930cd743312e3faaf1f
Giselle <gisecn@gmail.com>
2023-06-01 20:19:30 +0200

    updated button

--- pages/login.tsx
+++ pages/login.tsx
@@ -100,7 +100,7 @@ export default function Login() {
                 <a
                   href="https://calendly.com/cryptonikasdao/meeting-cks-founders"
                   target="_blank"
-                  className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2  dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+                  className="text-gray-300 hover:text-white border border-gray-300 hover:bg-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2  dark:border-gray-300 dark:text-gray-300 dark:hover:text-white dark:hover:bg-gray-600"
                 >
                   ¿Eres creador? <span aria-hidden="true">→</span>
                 </a>
